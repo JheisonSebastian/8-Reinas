f216344698922f131ef0fa85c005c2eeca7a2ddd
Jheison <jheison.gomez@uniminuto.edu.co>
2025-05-09 18:57:15 -0500

    modifico el modelo en base a la logica requerida, actualizo también el controlador y la vista

--- src/vista/VistaConsola.java
+++ src/vista/VistaConsola.java
@@ -1,18 +1,15 @@
 package vista;
 
 public class VistaConsola {
-
-    public void mostrarMensaje(String mensaje) {
-        System.out.println(mensaje);
-    }
-
     public void mostrarTablero(int[][] tablero) {
         for (int[] fila : tablero) {
             for (int celda : fila) {
-                System.out.print((celda == 1 ? "♛" : ".") + " ");
+                System.out.print((celda == 1 ? "R" : ".") + " ");
             }
             System.out.println();
         }
-        System.out.println(); // línea en blanco entre soluciones
+        System.out.println();
     }
-}
+
+    public void mostrarMensaje(String mensaje) {
+        System.out.println(mensaje);
